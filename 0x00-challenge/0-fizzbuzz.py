427b1882b35b2d2c9169aeb120c3776aea52d12b
nanyamax <ifunanyasunday7@gmail.com>
2023-02-25 22:49:01 -0800

    fixcode

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -22,14 +22,10 @@ def fizzbuzz(n):
         else:
             tmp_result.append(str(i))
     print(" ".join(tmp_result))
-    
+
 
 if __name__ == '__main__':
     if len(sys.argv) <= 1:
-        print("Missing number")
-        print("Usage: ./0-fizzbuzz.py <number>")
-        print("Example: ./0-fizzbuzz.py 89")
+        number = int(sys.argv[1])
+        fizzbuzz(number)
         sys.exit(1)
-
-    number = int(sys.argv[1])
-    fizzbuzz(number)
